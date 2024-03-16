1586e808a384656e384d0c4420e41f5d436c6352
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2024-03-16 21:34:38 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+# Update System And Add Required APT Repositories
+
+# Install Cinnamon DE
+
+# Install LightDM
+
+# Fix Ubiquity (Files From Ubiquity Repo Get Uploaded Here!)
+
+# Fix Plymouth Screens (Files From Plymouth Animation Repo Get Uploaded Here!)
+
+# Fix Default Background (Files From Assets Repo Get Uploaded Here!)
