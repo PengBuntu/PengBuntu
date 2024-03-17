ef3ff1f97d22d5cc1a900695c7deb370d7c3e24b
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2024-03-17 18:37:15 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,35 @@
 # Update System And Add Required APT Repositories
+apt update
+apt upgrade -y
+apt update
+add-apt-repository universe
 
 # Install Cinnamon DE
+apt install cinnamon
+update-alternatives --config x-session-manager
 
 # Install LightDM
+apt install lightdm
 
-# Fix Ubiquity (Files From Ubiquity Repo Get Uploaded Here!)
+# Fix Ubiquity
+cd /usr/share/ubiquity-slideshow/slides
+# (Upload/Replace Slides With Those From Ubiquity-Slides Repo)
 
-# Fix Plymouth Screens (Files From Plymouth Animation Repo Get Uploaded Here!)
+# Fix Plymouth Screens
+# (Upload Cantarell Font)
+mv Cantarell-Regular.ttf /usr/share/fonts
+ls -la /boot
+apt -y install --reinstall linux-headers-generic
+apt -y install --reinstall linux-image-generic
+ls -la /boot
+update-alternatives --display default.plymouth
+cd /usr/share/plymouth/themes
+mkdir logo-slider
+cd logo-slider
+# (Upload Plymouth Theme Files)
+update-alternatives --install "/usr/share/plymouth/themes/default.plymouth" "default.plymouth" "/usr/share/plymouth/themes/logo-slider/logo-slider.plymouth" 160
+update-alternatives --display default.plymouth
+update-initramfs -uk all
+# (Make Sure Cubic Uses This Modified Kernel!)
 
 # Fix Default Background (Files From Assets Repo Get Uploaded Here!)
